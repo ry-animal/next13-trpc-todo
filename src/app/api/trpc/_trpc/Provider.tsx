43affd1ba8dce299867a657661a4ab4fb3bd0954
Ryan Van Valkenburg <ryanlvv@gmail.com>
2023-08-21 17:28:51 -0700

    fix

--- src/app/api/trpc/_trpc/Provider.tsx
+++ src/app/api/trpc/_trpc/Provider.tsx
@@ -10,7 +10,7 @@ export default function Provider({ children }: {children: React.ReactNode}) {
     const [queryClient] = useState(() => new QueryClient({}));
     const [trpcClient] = useState(() => trpc.createClient({
         links: [httpBatchLink({ url: "/api/trpc" })],
-    }));
+}));
 
     return (
         <trpc.Provider client={trpcClient} queryClient={queryClient}>
